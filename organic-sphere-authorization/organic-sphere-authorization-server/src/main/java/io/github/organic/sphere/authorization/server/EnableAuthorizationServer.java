0f5c4589d99b3d5134e7a7a78b9a98bac43d4cf0
kurok1 <maimengzzz@gmail.com>
2022-11-15 16:10:10 +0800

    项目初始化

---
+++ organic-sphere-authorization/organic-sphere-authorization-server/src/main/java/io/github/organic/sphere/authorization/server/EnableAuthorizationServer.java
@@ -0,0 +1,15 @@
+package io.github.organic.sphere.authorization.server;
+
+import java.lang.annotation.*;
+
+/**
+ * TODO
+ *
+ * @author <a href="mailto:maimengzzz@gmail.com">韩超</a>
+ * @since 1.0.0
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+public @interface EnableAuthorizationServer {
+}
